6f68553591ce333e2c03e19fb5a59261d8595903
EternalDigitalAssets <eternaldigitalassets@gmail.com>
2023-11-16 19:46:27 +0200

    Added Video with Function and Style

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,3 +56,50 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+/**
+ * Video Play Button
+ */
+
+// Get the video element and play button
+const video = document.getElementById('aboutVideo');
+const playBtn = document.getElementById('playBtn');
+
+// Add click event listener to the play button
+playBtn.addEventListener('click', function () {
+  // Check if the video is paused or ended
+  if (video.paused || video.ended) {
+    // Play the video
+    video.play();
+    playBtn.classList.add('playing'); // Optional: Add a class for styling purposes
+  } else {
+    // Pause the video
+    video.pause();
+    playBtn.classList.remove('playing'); // Optional: Remove the class if it was added
+  }
+});
+
+// Add event listener to update the play button when the video is paused or ended
+video.addEventListener('pause', function () {
+  playBtn.classList.remove('playing');
+  playBtn.style.display = 'block'; // Show the play button
+});
+
+video.addEventListener('ended', function () {
+  playBtn.classList.remove('playing');
+  playBtn.style.display = 'block'; // Show the play button
+});
+
+// Add event listener to hide the play button when the video is playing
+video.addEventListener('play', function () {
+  playBtn.style.display = 'none'; // Hide the play button
+});
+
+// Add click event listener to the video element to pause the video
+video.addEventListener('click', function () {
+  if (!video.paused) {
+    video.pause();
+    playBtn.style.display = 'block'; // Show the play button when paused
+  }
+});
